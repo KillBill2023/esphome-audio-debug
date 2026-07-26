6f3403391019cf034bf10144a899024723ad2a00
KillBill2023 <129291281+KillBill2023@users.noreply.github.com>
2026-07-26 12:18:12 +0300

    Update __init__.py

--- components/audio_debug/__init__.py
+++ components/audio_debug/__init__.py
@@ -1,7 +1,23 @@
 import esphome.codegen as cg
+import esphome.config_validation as cv
+
+from esphome.const import CONF_ID
 
 CODEOWNERS = ["@KillBill2023"]
+
+AUTO_LOAD = []
 
 audio_debug_ns = cg.esphome_ns.namespace("audio_debug")
 
 AudioDebug = audio_debug_ns.class_("AudioDebug", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(AudioDebug),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
