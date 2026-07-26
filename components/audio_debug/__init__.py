97384e6f8af5211c8dd0d6ef129a64443a02be8a
KillBill2023 <129291281+KillBill2023@users.noreply.github.com>
2026-07-26 12:14:14 +0300

    Initialize audio_debug module with namespace and class

--- components/audio_debug/__init__.py
+++ components/audio_debug/__init__.py
@@ -1,1 +1,7 @@
+import esphome.codegen as cg
 
+CODEOWNERS = ["@KillBill2023"]
+
+audio_debug_ns = cg.esphome_ns.namespace("audio_debug")
+
+AudioDebug = audio_debug_ns.class_("AudioDebug", cg.Component)
