e5a54331bc7810a5d863ad4a0878df41c8237dc7
KillBill2023 <129291281+KillBill2023@users.noreply.github.com>
2026-07-27 20:09:48 +0300

    Add ESP32 heap memory monitoring
    Implement periodic heap memory logging in the audio debug component. Logs free heap and largest free block every 10 seconds on ESP32 to help monitor memory usage during audio operations. Also fixes formatting and properly closes namespaces.

--- components/audio_debug/audio_debug.cpp
+++ components/audio_debug/audio_debug.cpp
@@ -2,20 +2,34 @@
 
 #include "esphome/core/log.h"
 
+#ifdef USE_ESP32
+#include <esp_heap_caps.h>
+#endif
+
 namespace esphome {
 namespace audio_debug {
 
 static const char *const TAG = "audio_debug";
 
 void AudioDebug::setup() {
-
   ESP_LOGI(TAG, "Audio Debug started");
-
 }
 
 void AudioDebug::loop() {
+  uint32_t now = millis();
 
+  if (now - last_log_ < 10000)
+    return;
+
+  last_log_ = now;
+
+#ifdef USE_ESP32
+  ESP_LOGI(TAG,
+           "Heap: %u bytes, Largest block: %u bytes",
+           ESP.getFreeHeap(),
+           heap_caps_get_largest_free_block(MALLOC_CAP_8BIT));
+#endif
 }
 
-}
-}
+}  // namespace audio_debug
+}  // namespace esphome
--- components/audio_debug/audio_debug.h
+++ components/audio_debug/audio_debug.h
@@ -6,13 +6,12 @@ namespace esphome {
 namespace audio_debug {
 
 class AudioDebug : public Component {
-
  public:
-
   void setup() override;
-
   void loop() override;
 
+ protected:
+  uint32_t last_log_{0};
 };
 
 }  // namespace audio_debug
