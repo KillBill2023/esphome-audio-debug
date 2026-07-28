d00bf842006041edcc1fa8a552eee45651658015
KillBill2023 <129291281+KillBill2023@users.noreply.github.com>
2026-07-28 22:39:49 +0300

    Add microphone support to audio_debug

--- components/audio_debug/__init__.py
+++ components/audio_debug/__init__.py
@@ -2,22 +2,37 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 
 from esphome.const import CONF_ID
+from esphome.components import microphone
 
 CODEOWNERS = ["@KillBill2023"]
 
 AUTO_LOAD = []
 
+CONF_MICROPHONE = "microphone"
+
 audio_debug_ns = cg.esphome_ns.namespace("audio_debug")
 
 AudioDebug = audio_debug_ns.class_("AudioDebug", cg.Component)
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(AudioDebug),
-    }
-).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(AudioDebug),
+
+            cv.Required(CONF_MICROPHONE): cv.use_id(
+                microphone.Microphone
+            ),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
+
     await cg.register_component(var, config)
+
+    mic = await cg.get_variable(config[CONF_MICROPHONE])
+
+    cg.add(var.set_microphone(mic))
--- components/audio_debug/audio_debug.h
+++ components/audio_debug/audio_debug.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/components/microphone/microphone.h"
 
 namespace esphome {
 namespace audio_debug {
@@ -10,7 +11,13 @@ class AudioDebug : public Component {
   void setup() override;
   void loop() override;
 
+  void set_microphone(microphone::Microphone *mic) {
+    this->microphone_ = mic;
+  }
+
  protected:
+  microphone::Microphone *microphone_{nullptr};
+
   uint32_t last_log_{0};
 };
 
